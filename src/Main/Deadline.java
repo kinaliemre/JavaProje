2037b55a6cd04527823b0d449aaf972d8b03e794
XANATORR <emrecan5386@gmail.com>
2025-11-30 18:23:20 +0300

    Deadline sınıfı verileri yazıldı.Main class da olan gereksiz kodlar kaldırıldı.

--- src/Main/Deadline.java
+++ src/Main/Deadline.java
@@ -1,5 +1,28 @@
 package Main;
 
+import java.time.LocalDateTime;
+import java.time.Duration;
+
+
 public class Deadline {
+
+	private LocalDateTime dueDate;
+	
+	public Deadline(LocalDateTime dueDate) {
+		this.dueDate = dueDate;
+		
+	}
+	public LocalDateTime  getDueDate() {
+		return dueDate;
+	}
+	
+	public boolean isPast(LocalDateTime now) {
+        return now.isAfter(dueDate);
+		
+	}
+	
+	public long daysleft(LocalDateTime now) {
+		return Duration.between(now, dueDate).toDays();
+	}
 
 }
--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,18 +1,7 @@
 package Main;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
 
-        System.out.print("Bir tam sayı gir: ");
-        int sayi = scan.nextInt();
-   
-
-        System.out.println("Girdiğin sayı: " + sayi);
-    
-
-        scan.close();
     }
 }
