7deeefe75aeeaedbdde55036cf21e7448f71f3c6
XANATORR <emrecan5386@gmail.com>
2025-12-01 15:00:10 +0300

    Task sınıfının temel alanlarını ve tamamlanma durumunu eklendi.Ayrıca TimeTask sınıfı TimeAsk olrak değiştirildi.

--- src/Main/Task.java
+++ src/Main/Task.java
@@ -1,5 +1,35 @@
 package Main;
 
-public class Task {
+import java.time.LocalDateTime;
+
+public class Task implements Completable {
+	
+	private int id;
+	private String title;
+	private String description;
+	private boolean completed;
+	private int priority;
+	private Deadline deadline;
+	
+public Task(int id, String title, String description) {
+	this.id = id;
+	this.title = title;
+	this.description = description;
+	this.completed = false;
+	this.priority = 0;
+	this.deadline = null;
+	
+}
+
+@Override
+public void complete() {
+    this.completed = true;
+}
+
+@Override
+public boolean isCompleted() {
+    return completed;
+}
+
 
 }
