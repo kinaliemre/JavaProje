8bd99ec44abe15c092a701664625c17b784e4d12
XANATORR <emrecan5386@gmail.com>
2025-12-01 15:10:29 +0300

    Task sınıfı için getter ve setter metotlarını eklendi.

--- src/Main/Task.java
+++ src/Main/Task.java
@@ -30,6 +30,56 @@ public void complete() {
 public boolean isCompleted() {
     return completed;
 }
+public int getId() {
+    return id;
+}
+
+public String getTitle() {
+    return title;
+}
+
+public void setTitle(String title) {
+	this.title = title;
+}
+
+public String getDescription() {
+	return description;
+}
+
+public void setDescription(String description) {
+	this.description = description;
+}
+
+public int getPriority() {
+	return priority;
+}
+
+public void setPriority(int priority) {
+	this.priority = priority;
+}
+
+public Deadline getDeadline() {
+    return deadline;
+}
+
+public void setDeadline(Deadline deadline) {
+	this.deadline = deadline;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 }
