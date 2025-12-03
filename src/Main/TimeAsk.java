53bf352b4ad0f363ec31050886e8011a8c794a44
XANATORR <emrecan5386@gmail.com>
2025-12-03 10:05:22 +0300

    Zamanlanmış görevler için TimeAsk alt sınıfını eklendi.

--- src/Main/TimeAsk.java
+++ src/Main/TimeAsk.java
@@ -1,5 +1,21 @@
 package Main;
 
-public class TimeAsk {
+import java.time.LocalDateTime;
 
+
+public class TimeAsk extends Task {
+  
+	private LocalDateTime startTime;
+    private int durationMinutes;
+
+    public TimeAsk(int id, String title, String description, LocalDateTime startTime, int durationMinutes) {
+    
+    	super(id, title, description);
+    
+    	this.startTime = startTime;
+    
+    	this.durationMinutes = durationMinutes;
+    
+    
+    }
 }
