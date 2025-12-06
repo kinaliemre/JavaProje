614fc6a2c637099af7b28b8bfb00458cc5dee831
XANATORR <emrecan5386@gmail.com>
2025-12-06 16:11:08 +0300

    Görev hatırlatmaları için Notification sınıfını eklendi.

--- src/Main/Notification.java
+++ src/Main/Notification.java
@@ -1,5 +1,20 @@
 package Main;
 
-public class Notification {
+import java.time.LocalDateTime;
 
+public class Notification{
+	
+	private int id;
+	private String message;
+	private LocalDateTime notifyAt;
+	private Task task;
+	
+	public Notification(int id, String message, LocalDateTime notifAt, Task task) {
+		
+		this.id = id;
+		this.message = message;
+		this.notifyAt = notifyAt;
+		this.task = task;
+	}
+	
 }
