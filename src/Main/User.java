693a7d0889c57ed02def3589a85b0d91b907896e
XANATORR <emrecan5386@gmail.com>
2025-12-08 12:38:28 +0300

    Kullanıcıları temsil eden User sınıfını ve proje koleksiyonunu eklendi.

--- src/Main/User.java
+++ src/Main/User.java
@@ -1,5 +1,34 @@
 package Main;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
+	
+	private int id;
+	private String name;
+	private String email;
+	private List<Project> projects;
+	
+public User (int id, String name, String email) {
+	
+	this.id = id;
+	this.name = name;
+	this.email = email;
+	this.projects = new ArrayList<>();
+}
+
+public String getName() {
+	return name;
+}
+
+public void addProject(Project project) {
+	projects.add(project);
+}
+
+public List<Project> getProjects() {
+    return projects;
+}
 
 }
