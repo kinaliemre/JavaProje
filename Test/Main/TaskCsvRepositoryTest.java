634f50f599382af2b1caa1f3ff7c69706852d2c8
XANATORR <emrecan5386@gmail.com>
2025-12-14 15:21:37 +0300

    TaskCsvRepository test classı dosyası ayrıntılandırıldı.

--- Test/Main/TaskCsvRepositoryTest.java
+++ Test/Main/TaskCsvRepositoryTest.java
@@ -8,17 +8,18 @@ import java.nio.file.Path;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-
 import org.junit.jupiter.api.Test;
 
 public class TaskCsvRepositoryTest {
 
     @Test
-    void save_ve_load_ayni_gorev_listesini_dondurmeli() throws IOException {
+    void save_ve_load_ayni_sayida_gorev_dondurmeli() throws IOException {
         Task t1 = new Task(1, "Görev 1", "Açıklama 1");
+        t1.setPriority(2);
         t1.setDeadline(new Deadline(LocalDateTime.of(2025, 12, 10, 12, 0)));
 
         Task t2 = new Task(2, "Görev 2", "Açıklama 2");
+        t2.setPriority(5);
         t2.setDeadline(new Deadline(LocalDateTime.of(2025, 12, 11, 15, 0)));
 
         List<Task> tasks = new ArrayList<>();
@@ -34,5 +35,7 @@ public class TaskCsvRepositoryTest {
         assertEquals(2, loaded.size());
         assertEquals("Görev 1", loaded.get(0).getTitle());
         assertEquals("Görev 2", loaded.get(1).getTitle());
+        assertEquals(2, loaded.get(0).getPriority());
+        assertEquals(5, loaded.get(1).getPriority());
     }
 }
